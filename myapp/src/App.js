324d76aaa0c25a097bc75750be1eb1b9e16c7070
Miryalasundar <miriyallasundar@gmail.com>
2025-07-08 15:27:01 +0530

    initial commit

---
+++ myapp/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function parent() {
+  return (
+    <div>
+      <p>parent component</p>
+      <Child name={"aravind"}course={bca}/>
+    </div>
+  )
+}
